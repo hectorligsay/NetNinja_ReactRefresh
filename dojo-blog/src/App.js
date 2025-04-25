70345f2dbea54b68795890fecb7119800bbf713f
Hector Ligsay Jr <hligsay@aol.com>
2025-04-25 15:34:43 -0500

    On tutorial 6.

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 100;
+  const link = "https://www.google.com";
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+        {/* <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p> // number
+        <p> {"Hello Ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google Site</a> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
