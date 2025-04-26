bde9e204ebfd664d9b0d0fc86cca4cd1b24ffcc3
Hector Ligsay Jr <hligsay@aol.com>
2025-04-25 21:43:59 -0500

    Went over fetching with useEffect, hadnling fetch errors, and creating conditional loading messages.

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,26 +2,39 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum", author: "yoshi", id: 2 },
-    { title: "Web dev top tips", body: "lorem ipsum", author: "mario", id: 3 },
-  ]);
-
-  // This only returns a new filtered array based on the original one
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
+  const [blogs, setBlogs] = useState(null); // initialize the blogs array
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  // fetch data on initial render
+  // This will update our blogs useState
   useEffect(() => {
-    console.log("useEffect ran");
-    console.log(blogs);
-  }, [blogs]);
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+          console.log(err.name); // This is the name of the error (e.g., 'Error')
+          console.log(err.stack); // This is the stack trace (useful for debugging)
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
